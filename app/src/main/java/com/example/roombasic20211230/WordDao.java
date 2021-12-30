3ffdefa8adc75f7928a6cbbcec751f3eac4639fa
Sophie <wenxi1988@hotmail.com>
2021-12-30 16:50:13 -0500

    add crud methods for interface WordDao

--- app/src/main/java/com/example/roombasic20211230/WordDao.java
+++ app/src/main/java/com/example/roombasic20211230/WordDao.java
@@ -1,4 +1,30 @@
 package com.example.roombasic20211230;
 
-public interface WordDao {
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao // database access object
+public interface WordDao
+{
+    @Insert
+    void insertWords(Word... words) ;//... means pass more parameters
+    @Update
+    void updateWords(Word... words);
+    @Delete
+    void deleteWords(Word... words);
+    @Query("DELETE FROM WORD")
+    void deleteAllWords();
+    @Query("SELECT * FROM WORD ORDER BY ID DESC")
+    List<String> getAllWords();
+
+
+
+
+
+
 }
