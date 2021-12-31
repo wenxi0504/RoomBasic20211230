f4a736becc6b96c6892cf65e89261b6f481637c3
Sophie <wenxi1988@hotmail.com>
2021-12-30 22:53:43 -0500

    fix button bugs

--- app/src/main/java/com/example/roombasic20211230/MainActivity.java
+++ app/src/main/java/com/example/roombasic20211230/MainActivity.java
@@ -25,21 +25,67 @@ public class MainActivity extends AppCompatActivity {
         wordDatabase= Room.databaseBuilder(this,WordDatabase.class,"word_database")
                 .allowMainThreadQueries()// temporary use , normally crud should use at other treads
                 .build();
-
         wordDao=wordDatabase.getWordDao();
-       textView=findViewById(R.id.textView);
+        textView=findViewById(R.id.textView);
         buttonInsert=findViewById(R.id.buttonInsert);
+        buttonClear=findViewById(R.id.buttonClear);
+        buttonUpdate=findViewById(R.id.buttonUpdate);
+        buttonDelete=findViewById(R.id.buttonDelete);
+
+        /*
+        Word word1= new Word("hello","nihao");
+        Word word2= new Word("world","shijie");
+        Word word3= new Word("sophie","wen");
+        wordDao.insertWords(word1,word2,word3);
+         updateView();
+
+         */
+
         buttonInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Word word1= new Word("hello","nihao");
-                Word word2= new Word("world","shijie");
-                wordDao.insertWords(word1,word2);
+                Word word1= new Word("hello1","nihao");
+                Word word2= new Word("world1","shijie");
+                Word word3= new Word("sophie1","wen");
+                wordDao.insertWords(word1,word2,word3);
                 updateView();
 
             }
         });
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wordDao.deleteAllWords();
+                updateView();
+            }
+        });
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Word word = new Word("hi,", "nihaoa");
+                word.setId(1);
+                wordDao.updateWords(word);
+                updateView();
+
+
+            }
+        });
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Word word = new Word("hi,", "nihaoa");
+                word.setId(1);
+                wordDao.deleteWords(word);
+                updateView();
+            }
+        });
+
+
     }
+
 
     void updateView(){
         List<Word> list=wordDao.getAllWords();
@@ -47,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         for (int i=0;i<list.size();i++)
         {
             Word word=list.get(i);
-            text+=word.getId()+":"+word.getWord()+"="+word.getChineseMeaning();
+            text+=word.getId()+":"+word.getWord()+"="+word.getChineseMeaning()+'\n';
             textView.setText(text);
         }
     }
