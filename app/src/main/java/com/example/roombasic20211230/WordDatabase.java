c3a03f28ca47f5aca0b99bcb41b83a49d90a5017
Sophie <wenxi1988@hotmail.com>
2021-12-31 20:42:06 -0500

    create a singleton batabase

--- app/src/main/java/com/example/roombasic20211230/WordDatabase.java
+++ app/src/main/java/com/example/roombasic20211230/WordDatabase.java
@@ -1,9 +1,23 @@
 package com.example.roombasic20211230;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+//singleton let a class only can create one instance
 @Database(entities ={Word.class},version=1,exportSchema = false )
 public abstract class WordDatabase extends RoomDatabase {
+    private  static WordDatabase INSTANCE;
+    //singleton 
+    static synchronized WordDatabase getDatabase(Context context){
+        if(INSTANCE == null)
+        {
+            INSTANCE= Room.databaseBuilder(context.getApplicationContext(),WordDatabase.class,"word_database")
+                    .allowMainThreadQueries().build();
+        }
+        return INSTANCE;
+    }
     public abstract WordDao getWordDao();
 }
